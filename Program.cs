63356bbb56e20c8c75ab670e9fed89b4dd098d37
Isaiah-Cargill-student <isca71b@student.oaisd.org>
2024-10-10 14:11:25 +0000

    My put my name in and put my age in

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("my name is Isaiah Cargill.");
+
+        int age = 17;
+        string greeting = "hello world";
+        string firstname = "Isaiah";
+        string lastname = "Cargill";
+        double money = 2.79;
+
+       Console.WriteLine(greeting+ " ! " + "<My name is " + firstname + " "
+       + lastname + ". I am " + age + " years old.  I just spent $" + money +"on an Energy Drink."); 
+    }
+}
